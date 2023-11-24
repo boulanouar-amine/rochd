78871290527372ee0f1ea8051f52e0270f1a3cd6
boulanouar mohamed amine <86970749+boulanouar-amine@users.noreply.github.com>
2023-11-24 09:07:59 +0100

    modified style
    Co-authored-by: yobahadda <yobahadda@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
-from flask import Flask,render_template,request,redirect,url_for,session,flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    return render_template("admin.html")
+
+
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    return render_template("user.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
-    
